cedd6d28e89191fc547814178758ac5cda02392c
Oliver Degener <o.degener@monday-consulting.com>
2015-03-25 14:19:03 +0100

    added OpenIncognitoActivity (intent filter for http/https urls)

---
+++ app/src/main/java/org/ollide/xposed/incognitotab/OpenIncognitoActivity.java
@@ -0,0 +1,34 @@
+package org.ollide.xposed.incognitotab;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class OpenIncognitoActivity extends Activity {
+
+    private static final String CHROME_EMPTY_TAB = "googlechrome://navigate?url=chrome-native://newtab/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent in = getIntent();
+        if (Intent.ACTION_VIEW.equals(in.getAction())) {
+
+            Intent incognitoIntent = new Intent(Intent.ACTION_VIEW);
+            incognitoIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+            incognitoIntent.setData(Uri.parse(CHROME_EMPTY_TAB));
+
+            try {
+                startActivity(incognitoIntent);
+            } catch (ActivityNotFoundException exception) {
+                Toast.makeText(this, R.string.chrome_not_found, Toast.LENGTH_SHORT).show();
+            }
+
+            finish();
+        }
+    }
+}
